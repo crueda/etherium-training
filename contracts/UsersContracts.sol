58c5d6d3cd49820e0a8d16e8b84c96deefc84b13
Carlos Rueda Morales <crueda@gmail.com>
2018-10-24 18:17:16 +0200

    cambios

--- contracts/UsersContracts.sol
+++ contracts/UsersContracts.sol
@@ -8,6 +8,16 @@ contract UsersContracts {
     }
 
     mapping(address => User) private users;
-}
 
-function join (string name, string surname)
+    function join (string name, string surname) public {
+        User storage user = users[msg.sender];
+        user.name = name;
+        user.surname = surname;
+    }
+
+    function getUser(address addr) public view returns (string, string) {
+        User memory user = users[addr];
+        return (user.name, user.surname);
+    }
+
+}
