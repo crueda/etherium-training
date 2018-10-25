134e3bd625c0cfd47b33a9da92dce8d8a0470fb9
Carlos Rueda Morales <crueda@gmail.com>
2018-10-25 20:17:06 +0200

    cambios

--- contracts/UsersContracts.sol
+++ contracts/UsersContracts.sol
@@ -8,16 +8,29 @@ contract UsersContracts {
     }
 
     mapping(address => User) private users;
+    mapping(address => bool) private joinedUsers;
+    address[] total;
 
     function join (string name, string surname) public {
+        require(!userJoined(msg.sender));
         User storage user = users[msg.sender];
         user.name = name;
         user.surname = surname;
+        joinedUsers[msg.sender] = true;
+        total.push(msg.sender);
     }
 
     function getUser(address addr) public view returns (string, string) {
+        require(userJoined(msg.sender));
         User memory user = users[addr];
         return (user.name, user.surname);
     }
 
+    function userJoined(address addr) private view returns (bool) {
+        return joinedUsers[addr];
+    }
+
+    function totalUsers() public view returns (uint) {
+        return total.length;
+    }
 }
